6d8154907d8143b2056023a2eb68fb7bf484c879
Nikias Tacain <nikiastacain@yahoo.com.ar>
2023-09-02 19:27:17 -0300

    prueba 2

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,31 +5,15 @@ import React from 'react';
 import './globals.css';
 import { CarritoProvider } from './tienda/components/CarritoContext';
 export default function RootLayout({ children }) {
-  const metadata = {
-    title: 'Deal Dress',
-    description: 'Tienda web donde se venden productos deportivos. Ropa, herramientas, pesas, etc.',
-    icons:{
-      icon:[
-        '/favicon.ico'
-      ],
-      apple:[
-        '/apple-touch-icon.png'
-      ],
-      shortcut:[
-        '/apple-touch-icon.png'
-      ],
-      manifest:'/site.webmanifest'
-    }
-  }
   return (
     <CarritoProvider>
-    <html lang="es">
-      <body>
-        <HeaderHub />
-        {children}
-        <FooterHub />
-      </body>
-    </html>
+      <html lang="es">
+        <body>
+          <HeaderHub />
+          {children}
+          <FooterHub />
+        </body>
+      </html>
     </CarritoProvider>
     
   )
