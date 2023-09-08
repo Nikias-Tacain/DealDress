4cfe7c3f0a00c284b869ac9bfa04776a9c7a0a0e
Nikias Tacain <nikiastacain@yahoo.com.ar>
2023-09-08 11:21:09 -0300

    diseño

--- src/app/components/FooterHub.jsx
+++ src/app/components/FooterHub.jsx
@@ -12,7 +12,7 @@ export default function FooterHub() {
                 </article>
                 <article>
                     <p>Nos podes encontrar en:</p>
-                    <a className={styles.linkSocialMedia} href="https://www.instagram.com/dealdress.tiendaonline/" target='_blank'><Image src="/instagram.svg" alt="Estamos en Instagram" width={200} height={60}/></a>
+                    <a className={styles.linkSocialMedia} href="https://www.instagram.com/dealdress.tiendaonline/" target='_blank'><Image src="/instagram.svg" alt="Estamos en Instagram" width={200} height={60} /></a>
                     <a className={styles.linkSocialMedia} href="https://www.tiktok.com/@dealdressropa" target='_blank'><Image src="/tiktok.svg" alt="Estamos en TikTok" width={200} height={60}/></a>
                     <a className={styles.linkSocialMedia} href="https://www.facebook.com/people/dealdresstiendaonline/100063642266774/" target='_blank'><Image src="/facebook.svg" alt="Estamos en Facebook" width={200} height={60}/></a>
                     <a className={styles.linkSocialMedia} href="https://api.whatsapp.com/send/?phone=5493415075439&text&type=phone_number&app_absent=0" target='_blank'><Image src="/whatsapp.svg" alt="Estamos en WhatsApp" width={200} height={60}/></a>
--- src/app/tienda/page.jsx
+++ src/app/tienda/page.jsx
@@ -2,18 +2,17 @@ import TiendaNavBar from "./components/TiendaNavBar";
 import ProductList from "./components/allProducts";
 import CarrouselImgs from "./components/CarrouselImgs";
 import FloatingWhatsAppButton from "./components/FloatingWhatsAppButton";
-import styles from './page.module.css'
 
 function page() {
     return(
-        <section className={styles.page}>
+        <>
             <CarrouselImgs />
             <TiendaNavBar />
             <div>
                 <ProductList/>
             </div>
             <FloatingWhatsAppButton />
-        </section>
+        </>
     )
 }
 export default page;
