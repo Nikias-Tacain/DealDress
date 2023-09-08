ef4b478bc329d5876b253b14dbf094958bcd2448
Nikias Tacain <nikiastacain@yahoo.com.ar>
2023-09-08 11:41:07 -0300

    prueba v2

--- src/app/components/HeaderHubCarrousel.jsx
+++ src/app/components/HeaderHubCarrousel.jsx
@@ -4,7 +4,7 @@ export default function HeaderHubCarrousel() {
     return(
         <section>
             <div  className={styles.carrouselImg}>
-                <Image src="/carrouselImg.png" alt="Imagen portada" width={1000} height={500}/>
+                <Image src="/carrouselImg.webp" alt="Imagen portada" width={1000} height={500}/>
             </div>
             <marquee behavior="scroll" direction="left" className={styles.headerHubCarrouselMarquee}>
                 ENVIAMOS A TODO EL PAIS A DOMICILIO CON ANDREANI !!! 🚛🚛🚛 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
