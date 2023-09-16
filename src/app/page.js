05c7b4c519f0f5487dc8ea2b7c7f48b76d054359
Nikias Tacain <nikiastacain@yahoo.com.ar>
2023-09-16 12:43:08 -0300

    prueba archivos

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import '@fortawesome/fontawesome-svg-core/styles.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import ProductsHub from './components/ProductsHub';
 import FloatingWhatsAppButton from './tienda/components/FloatingWhatsAppButton';
-import CorreoHome from './components/correoHome';
+import CorreoHome from './components/CorreoHome';
 config.autoAddCss = false;
 export default function page() {
     return(
